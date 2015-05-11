44768971ec578bd42bd64f5032b2524efc88f6f9
dwxavier <dwxavier@tiexpert.net>
2015-05-11 11:47:39 -0300

    ReCaptchaV2 class documentation
    Added in code documentation for ReCaptchaV2 class.
    Added License file in main folder for distribution.
    Added a commented line for people who are having trouble using the cURL
    module in isValid() method.

--- reCaptchaV2/reCaptchaV2.php
+++ reCaptchaV2/reCaptchaV2.php
@@ -16,18 +16,50 @@
 */
 require_once ("reCaptchaV2I18n.php");
 
-class ReCaptchaV2 {	
+/**
+* The main class of ReCaptchaV2 library.
+*
+* Takes care of the widget rendering, its validation and treats any error replied by Google reCAPTCHA.
+*
+* @author Denys W. Xavier
+* @version 1.0.0
+* @license Apache License 2.0
+*/
+class ReCaptchaV2 {
+	/** Request Url for Google API. */
 	const API_URL = "https://www.google.com/recaptcha/api";
 
+	/** @var ReCaptchaV2I18n $i18n Internationalization definitions for ReCaptchaV2. */
 	private $i18n;
-	private $includeNoScript;
+	/** @var bool $includeNoScript Indicates whether or not including noscript tag along with the widget tag. */
+	private $includeNoScript = false;
+	/** @var array $lastErrors Last validation errors cache. */
 	private $lastErrors = NULL;
+	/** @var string $secretKey Secret key (or private key) provided by Google. */
 	private $secretKey;
+	/** @var string $siteKey Site key (or public key) provided by Google. */
 	private $siteKey;
-	private $theme;
-	private $type;
+	/** @var string $theme Theme to be used by the widget. */
+	private $theme = "light";
+	/** @var string $type Widget type to be used. */
+	private $type = "image";
+	/** @var bool $valid Validation cache. */
 	private $valid = NULL;
 
+	/**
+	* Creates a new instance of ReCaptchaV2.
+	*
+	* @param string $siteKey
+	* @param string $secretKey
+	* @param string $theme
+	* @param string $type
+	* @param string $lang
+	* @param bool $includeNoScript 
+	*
+	* @return self
+	*
+	* @throws InvalidArgumentException If any key is not provided.
+	*/
 	public function __construct ($siteKey, $secretKey, $theme = "light", $type = "image", $lang = NULL, $includeNoScript = false) {
 		$this->i18n = new ReCaptchaV2I18n($lang);
 		
@@ -41,14 +73,49 @@ class ReCaptchaV2 {
 		}
 	}
 
+	/**
+	* Gets the language code currently used.
+	*
+	* @uses ReCaptchaV2I18n
+	* 
+	* @return string
+	*/
 	public function getLang() {
 		return $this->i18n->getLang();
+	}	
+
+	/**
+	* Gets an array containing the error codes.
+	*
+	* @return array
+	*/
+	public function getLastErrorCodes() {
+		return array_keys($this->lastErrors);
 	}
 
+	/**
+	* Gets an array containing the error descriptions.
+	*
+	* @return array
+	*/
+	public function getLastErrorDescriptions() {
+		return array_values($this->lastErrors);
+	}
+
+	/**
+	* Gets an associative array containing the last error codes replied and their descriptions.
+	*
+	* @return array
+	*/
 	public function getLastErrors() {
 		return $this->lastErrors;
 	}
 
+	/**
+	* Gets a string containing all error descriptions separated by a semicolon (;).
+	*
+	* @return string
+	*/
 	public function getLastErrorsAsString() {
 		if (is_array($this->lastErrors)) {
 			return implode("; ", $this->getLastErrorDescriptions());
@@ -56,14 +123,13 @@ class ReCaptchaV2 {
 		return "";
 	}
 	
-	public function getLastErrorCodes() {
-		return array_keys($this->lastErrors);
-	}
-
-	public function getLastErrorDescriptions() {
-		return array_values($this->lastErrors);
-	}
-
+	/**
+	* Gets a string that represents the script tag that refers to the Google API.
+	*
+	* @uses ReCaptchaI18n
+	*
+	* @return string
+	*/
 	public function getScript() {
 		$jsURL = self::API_URL . ".js";
 		$urlVars = array();
@@ -78,15 +144,29 @@ class ReCaptchaV2 {
 		
 		return '<script src="' . $jsURL . '" type="text/javascript" async="async" defer="defer"></script>';
 	}
-
+	/**
+	* Gets the theme currently used by the widget.
+	*
+	* @return string
+	*/
 	public function getTheme() {
 		return $this->theme;
 	}
-	
+
+	/**
+	* Gets the type of widget currently used.
+	*
+	* @return string
+	*/
 	public function getType() {
 		return $this->type;
 	}
-	
+
+	/**
+	* Gets a string containing the necessary tags for rendering the widget.
+	*
+	* @return string
+	*/
 	public function getWidget() {
 		$wHTML = '<div class="g-recaptcha" data-sitekey="' . $this->siteKey . '"';
 		
@@ -117,7 +197,14 @@ class ReCaptchaV2 {
 		}
 		return $wHTML;
 	}
-	
+
+	/**
+	* Validates the widget response.
+	*
+	* @return bool
+	*
+	* @throws Exception If any error occur during the execution of cURL module
+	*/
 	public function isValid(){
 		if (is_null($this->valid)) {
 			$data["secret"] = $this->secretKey;
@@ -129,6 +216,7 @@ class ReCaptchaV2 {
 			curl_setopt($cUrl, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($cUrl, CURLOPT_POST, true);
 			curl_setopt($cUrl, CURLOPT_POSTFIELDS, http_build_query($data));
+			//curl_setopt($cUrl, CURLOPT_SSL_VERIFYPEER, false);
 			$response = json_decode(curl_exec($cUrl));		
 			if (curl_errno($cUrl) > 0) {
 				throw new Exception(curl_error($cUrl), curl_errno($cUrl));
@@ -146,15 +234,37 @@ class ReCaptchaV2 {
 		return $this->valid;
 	}
 
+	/**
+	* Sets the language to be used for internationalization.
+	*
+	* @param string $lang Language code to be used.
+	*
+	* @uses ReCaptchaI18n
+	*
+	* @return self
+	*/
 	public function setLang($lang) {		
 		return $this->i18n->setLang($lang);
 	}
 
+	/**
+	* Sets the theme to be used by the widget.
+	*
+	* @param string $theme Widget theme name to be used. It can be "light" or "dark".
+	* @return self
+	*/
 	public function setTheme($theme) {
 		$this->theme = $theme;
 		return $this;
 	}
 
+	/**
+	* Define o tipo de widget a ser utilizado.
+	*
+	* @param string $type Widget type name to be used. It can be "image" or "audio".
+	* 
+	* @return self
+	*/
 	public function setType($type) {
 		$this->type = $type;
 		return $this;
